65ead72cfc78a8dc8baa3dd76957ae56b340f673
Bpmauge <briahanamauge@pursuit.org>
2020-02-03 21:14:43 -0500

    commented out the creste and drop of database

--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,9 @@
-DROP DATABASE IF EXISTS booke;
-CREATE DATABASE booke;
+-- DROP DATABASE IF EXISTS booke;
+-- CREATE DATABASE booke;
 
-\c booke
+-- \c booke
+DROP TABLE IF EXISTS bookmarks;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id serial,
@@ -9,6 +11,7 @@ CREATE TABLE users (
     username VARCHAR(50),
     PRIMARY KEY (id)
 );
+
 
 CREATE TABLE bookmarks (
     id serial PRIMARY KEY,
